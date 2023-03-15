1d57c0c33e8fb05bf33f1fb09fc5fcb45991f50c
bruno-sena-evercast <bruno.sena@evercast.us>
2023-03-15 15:02:47 -0300

    Add benchmark project

---
+++ benchmark/main.cpp
@@ -0,0 +1,289 @@
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include "Windows.h"
+
+#include "renderdoc_app.h"
+
+#include "Device.h"
+
+using namespace PixelWeave;
+
+struct Timer {
+    void Start() { beginTime = std::chrono::steady_clock::now(); }
+    template <typename M>
+    uint64_t Elapsed()
+    {
+        return std::chrono::duration_cast<M>(std::chrono::steady_clock::now() - beginTime).count();
+    }
+    uint64_t ElapsedMillis() { return Elapsed<std::chrono::milliseconds>(); }
+    uint64_t ElapsedMicros() { return Elapsed<std::chrono::microseconds>(); }
+
+    std::chrono::steady_clock::time_point beginTime;
+};
+
+PixelWeave::VideoFrameWrapper GetUYVYFrame(uint32_t width, uint32_t height)
+{
+    const uint32_t stride = width * 2;
+    const uint32_t bufferSize = height * stride;
+    uint8_t* buffer = new uint8_t[bufferSize];
+    for (uint32_t x = 0; x < ((width + 1) / 2); ++x) {
+        for (uint32_t y = 0; y < height; ++y) {
+            const uint32_t baseIndex = y * stride + x * 4;
+            buffer[baseIndex] = 0xB0;      // U
+            buffer[baseIndex + 1] = 0xFF;  // Y
+            buffer[baseIndex + 2] = 0xC0;  // V
+            buffer[baseIndex + 3] = 0xFF;  // Y
+        }
+    }
+    return VideoFrameWrapper{buffer, stride, width, height, PixelWeave::PixelFormat::Interleaved8BitUYVY};
+}
+
+PixelWeave::VideoFrameWrapper GetPlanar420Frame(uint32_t width, uint32_t height)
+{
+    const uint32_t chromaWidth = (width + 1) / 2;
+    const uint32_t chromaHeight = (height + 1) / 2;
+    const uint32_t bufferSize = (height * width) + chromaWidth * chromaHeight * 2;
+    uint8_t* buffer = new uint8_t[bufferSize];
+    for (uint32_t ySampleIndex = 0; ySampleIndex < width * height; ++ySampleIndex) {
+        buffer[ySampleIndex] = 0xFF;
+    }
+    const uint32_t uSampleOffset = width * height;
+    const uint32_t vSampleOffset = uSampleOffset + chromaWidth * chromaHeight;
+    for (uint32_t uSampleIndex = 0; uSampleIndex < chromaWidth * chromaHeight; ++uSampleIndex) {
+        buffer[uSampleOffset + uSampleIndex] = 0xFF;
+    }
+    for (uint32_t vSampleIndex = 0; vSampleIndex < chromaWidth * chromaHeight; ++vSampleIndex) {
+        buffer[vSampleOffset + vSampleIndex] = 0xFF;
+    }
+    return VideoFrameWrapper{buffer, width, width, height, PixelWeave::PixelFormat::Planar8Bit420, PixelWeave::Range::Limited};
+}
+
+PixelWeave::VideoFrameWrapper GetPlanar422Frame(uint32_t width, uint32_t height)
+{
+    const uint32_t stride = width;
+    const uint32_t bufferSize = height * stride * 2;
+    uint8_t* buffer = new uint8_t[bufferSize];
+    for (uint32_t bufferIndex = 0; bufferIndex < bufferSize; ++bufferIndex) {
+        buffer[bufferIndex] = 0;
+    }
+    return VideoFrameWrapper{buffer, stride, width, height, PixelWeave::PixelFormat::Planar8Bit422};
+}
+
+PixelWeave::VideoFrameWrapper GetPlanar444Frame(uint32_t width, uint32_t height)
+{
+    const uint32_t stride = width;
+    const uint32_t bufferSize = height * stride * 3;
+    uint8_t* buffer = new uint8_t[bufferSize];
+    for (uint32_t bufferIndex = 0; bufferIndex < bufferSize; ++bufferIndex) {
+        buffer[bufferIndex] = 0;
+    }
+    return VideoFrameWrapper{buffer, stride, width, height, PixelWeave::PixelFormat::Planar8Bit444};
+}
+
+PixelWeave::VideoFrameWrapper GetPlanarYV12Frame(uint32_t width, uint32_t height)
+{
+    const uint32_t chromaWidth = (width + 1) / 2;
+    const uint32_t chromaHeight = (height + 1) / 2;
+    const uint32_t bufferSize = (height * width) + chromaWidth * chromaHeight * 2;
+    uint8_t* buffer = new uint8_t[bufferSize];
+    for (uint32_t ySampleIndex = 0; ySampleIndex < width * height; ++ySampleIndex) {
+        buffer[ySampleIndex] = 0xFF;
+    }
+    const uint32_t vSampleOffset = width * height;
+    const uint32_t uSampleOffset = vSampleOffset + chromaWidth * chromaHeight;
+
+    for (uint32_t vSampleIndex = 0; vSampleIndex < chromaWidth * chromaHeight; ++vSampleIndex) {
+        buffer[vSampleOffset + vSampleIndex] = 0xC0;
+    }
+    for (uint32_t uSampleIndex = 0; uSampleIndex < chromaWidth * chromaHeight; ++uSampleIndex) {
+        buffer[uSampleOffset + uSampleIndex] = 0xB0;
+    }
+    return VideoFrameWrapper{buffer, width, width, height, PixelWeave::PixelFormat::Planar8Bit420YV12};
+}
+
+PixelWeave::VideoFrameWrapper GetPlanarNV12Frame(uint32_t width, uint32_t height)
+{
+    const uint32_t chromaWidth = (width + 1) / 2;
+    const uint32_t chromaHeight = (height + 1) / 2;
+    const uint32_t bufferSize = (height * width) + chromaWidth * chromaHeight * 2;
+    uint8_t* buffer = new uint8_t[bufferSize];
+    for (uint32_t ySampleIndex = 0; ySampleIndex < width * height; ++ySampleIndex) {
+        buffer[ySampleIndex] = 0xFF;
+    }
+    const uint32_t uvSampleOffset = width * height;
+    for (uint32_t uvSampleIndex = 0; uvSampleIndex < 2 * chromaWidth * chromaHeight; uvSampleIndex += 2) {
+        buffer[uvSampleOffset + uvSampleIndex] = 0xB0;
+        buffer[uvSampleOffset + uvSampleIndex + 1] = 0xC0;
+    }
+    return VideoFrameWrapper{buffer, width, width, height, PixelWeave::PixelFormat::Planar8Bit420NV12};
+}
+
+PixelWeave::VideoFrameWrapper GetRGBAFrame(uint32_t width, uint32_t height)
+{
+    const uint32_t bufferSize = (height * width) * 4;
+    uint8_t* buffer = new uint8_t[bufferSize];
+    for (uint32_t sampleIndex = 0; sampleIndex < width * height; ++sampleIndex) {
+        buffer[sampleIndex * 4] = 0xFF;
+        buffer[sampleIndex * 4 + 1] = 0xFF;
+        buffer[sampleIndex * 4 + 2] = 0xFF;
+        buffer[sampleIndex * 4 + 3] = 0xFF;
+    }
+    return VideoFrameWrapper{buffer, width * 4, width, height, PixelWeave::PixelFormat::Interleaved8BitRGBA, PixelWeave::Range::Full};
+}
+
+PixelWeave::VideoFrameWrapper GetPlanar42010BitFrame(uint32_t width, uint32_t height)
+{
+    const uint32_t chromaWidth = (width + 1) / 2;
+    const uint32_t chromaHeight = (height + 1) / 2;
+    const uint32_t bufferSize = 2 * ((height * width) + chromaWidth * chromaHeight * 2);
+    uint16_t* buffer = new uint16_t[bufferSize];
+    for (uint32_t ySampleIndex = 0; ySampleIndex < width * height; ++ySampleIndex) {
+        buffer[ySampleIndex] = 0;
+    }
+    const uint32_t uSampleOffset = width * height;
+    const uint32_t vSampleOffset = uSampleOffset + chromaWidth * chromaHeight;
+    for (uint32_t uSampleIndex = 0; uSampleIndex < chromaWidth * chromaHeight; ++uSampleIndex) {
+        buffer[uSampleOffset + uSampleIndex] = 0;
+    }
+    for (uint32_t vSampleIndex = 0; vSampleIndex < chromaWidth * chromaHeight; ++vSampleIndex) {
+        buffer[vSampleOffset + vSampleIndex] = 0;
+    }
+    return VideoFrameWrapper{
+        reinterpret_cast<uint8_t*>(buffer),
+        width * 2,
+        width,
+        height,
+        PixelWeave::PixelFormat::Planar10Bit420,
+        PixelWeave::Range::Full};
+}
+
+PixelWeave::VideoFrameWrapper GetPlanar42210BitFrame(uint32_t width, uint32_t height)
+{
+    const uint32_t chromaWidth = (width + 1) / 2;
+    const uint32_t chromaHeight = height;
+    const uint32_t bufferSize = 2 * ((height * width) + chromaWidth * chromaHeight * 2);
+    uint16_t* buffer = new uint16_t[bufferSize];
+    for (uint32_t ySampleIndex = 0; ySampleIndex < width * height; ++ySampleIndex) {
+        buffer[ySampleIndex] = 0;
+    }
+    const uint32_t uSampleOffset = width * height;
+    const uint32_t vSampleOffset = uSampleOffset + chromaWidth * chromaHeight;
+    for (uint32_t uSampleIndex = 0; uSampleIndex < chromaWidth * chromaHeight; ++uSampleIndex) {
+        buffer[uSampleOffset + uSampleIndex] = 0;
+    }
+    for (uint32_t vSampleIndex = 0; vSampleIndex < chromaWidth * chromaHeight; ++vSampleIndex) {
+        buffer[vSampleOffset + vSampleIndex] = 0;
+    }
+    return VideoFrameWrapper{reinterpret_cast<uint8_t*>(buffer), width * 2, width, height, PixelWeave::PixelFormat::Planar10Bit422};
+}
+
+PixelWeave::VideoFrameWrapper GetPlanar44410BitFrame(uint32_t width, uint32_t height)
+{
+    const uint32_t chromaWidth = width;
+    const uint32_t chromaHeight = height;
+    const uint32_t bufferSize = 2 * ((height * width) + chromaWidth * chromaHeight * 2);
+    uint16_t* buffer = new uint16_t[bufferSize];
+    for (uint32_t ySampleIndex = 0; ySampleIndex < width * height; ++ySampleIndex) {
+        buffer[ySampleIndex] = 0;
+    }
+    const uint32_t uSampleOffset = width * height;
+    const uint32_t vSampleOffset = uSampleOffset + chromaWidth * chromaHeight;
+    for (uint32_t uSampleIndex = 0; uSampleIndex < chromaWidth * chromaHeight; ++uSampleIndex) {
+        buffer[uSampleOffset + uSampleIndex] = 0;
+    }
+    for (uint32_t vSampleIndex = 0; vSampleIndex < chromaWidth * chromaHeight; ++vSampleIndex) {
+        buffer[vSampleOffset + vSampleIndex] = 0;
+    }
+    return VideoFrameWrapper{reinterpret_cast<uint8_t*>(buffer), width * 2, width, height, PixelWeave::PixelFormat::Planar10Bit444};
+}
+
+PixelWeave::VideoFrameWrapper Get10BitRGBBuffer(uint32_t width, uint32_t height)
+{
+    const uint32_t bufferSize = width * height;
+    uint32_t* buffer = new uint32_t[bufferSize];
+    for (uint32_t bufferIndex = 0; bufferIndex < bufferSize; ++bufferIndex) {
+        buffer[bufferIndex] = 0;
+    }
+    return VideoFrameWrapper{reinterpret_cast<uint8_t*>(buffer), width * 4, width, height, PixelWeave::PixelFormat::Interleaved10BitRGB};
+}
+
+PixelWeave::VideoFrameWrapper GetPlanarP126Frame(uint32_t width, uint32_t height)
+{
+    const uint32_t chromaWidth = (width + 1) / 2;
+    const uint32_t chromaHeight = height;
+    const uint32_t bufferSize = height * width + chromaWidth * chromaHeight * 2;
+    uint16_t* buffer = new uint16_t[bufferSize];
+    for (uint32_t ySampleIndex = 0; ySampleIndex < width * height; ++ySampleIndex) {
+        buffer[ySampleIndex] = 0x00FF;
+    }
+    const uint32_t uvSampleOffset = width * height;
+
+    for (uint32_t lineIndex = 0; lineIndex < chromaHeight; ++lineIndex) {
+        const uint16_t sample = lineIndex % 2 == 1 ? 0 : 0xFFFF;
+        const uint32_t lineOffset = chromaWidth * 2 * lineIndex;
+        for (uint32_t uvSampleIndex = 0; uvSampleIndex < chromaWidth * 2; uvSampleIndex += 2) {
+            buffer[uvSampleOffset + lineOffset + uvSampleIndex] = sample;
+            buffer[uvSampleOffset + lineOffset + uvSampleIndex + 1] = sample;
+        }
+    }
+
+    return VideoFrameWrapper{reinterpret_cast<uint8_t*>(buffer), width * 2, width, height, PixelWeave::PixelFormat::Planar16BitP216};
+}
+
+int main()
+{
+    RENDERDOC_API_1_5_0* rdoc_api = nullptr;
+    if (HMODULE mod = GetModuleHandleA("renderdoc.dll")) {
+        pRENDERDOC_GetAPI RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)GetProcAddress(mod, "RENDERDOC_GetAPI");
+        if (RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_5_0, (void**)&rdoc_api) != 1) {
+            rdoc_api = nullptr;
+        }
+    }
+    if (rdoc_api == nullptr) {
+        std::cout << "Cannot initalize RenderDoc" << std::endl;
+    }
+    if (rdoc_api) {
+        std::cout << "Start RenderDoc capture" << std::endl;
+        rdoc_api->StartFrameCapture(NULL, NULL);
+    }
+
+    auto [result, device] = PixelWeave::Device::Create();
+    if (result == PixelWeave::Result::Success) {
+        constexpr uint32_t srcWidth = 1920;
+        constexpr uint32_t srcHeight = 1080;
+        VideoFrameWrapper srcFrame = GetPlanarP126Frame(srcWidth, srcHeight);
+        constexpr uint32_t dstWidth = 1920;
+        constexpr uint32_t dstHeight = 1080;
+        VideoFrameWrapper dstFrame = GetPlanar422Frame(dstWidth, dstHeight);
+
+        const auto videoConverter = device->CreateVideoConverter();
+        uint64_t totalTime = 0;
+        const int totalFrames = 10000;
+        for (int i = 0; i < totalFrames; ++i) {
+            Timer timer;
+            timer.Start();
+            if (videoConverter->Convert(srcFrame, dstFrame) != PixelWeave::Result::Success) {
+                std::cout << "Conversion failed" << std::endl;
+            }
+            totalTime += timer.ElapsedMicros();
+            std::cout << "Processing frame " << i << " took " << timer.ElapsedMillis() << "ms (" << timer.ElapsedMicros() << " us)"
+                      << std::endl;
+        }
+        std::cout << "Average time: " << static_cast<double>(totalTime) / (1000.0 * static_cast<double>(totalFrames)) << " ms" << std::endl;
+
+        videoConverter->Release();
+        device->Release();
+
+        delete[] srcFrame.buffer;
+        delete[] dstFrame.buffer;
+
+        if (rdoc_api)
+            rdoc_api->EndFrameCapture(NULL, NULL);
+
+        return 0;
+    }
+    return -1;
+}
